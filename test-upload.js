07f8d4aa111be1fc39e917816abcd9e83641b407
Dustin Moore <dustmoo@gmail.com>
2026-05-18 14:52:18 -0700

    docs: add FOSS MIT license and comprehensive README documenting architecture and branch lifecycle

--- test-upload.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-fetch('http://localhost:9002/api/upload', {
-  method: 'POST',
-  headers: {
-    'X-CSRF-Token': 'test'
-  },
-  body: new FormData()
-}).then(res => res.text()).then(console.log).catch(console.error);
